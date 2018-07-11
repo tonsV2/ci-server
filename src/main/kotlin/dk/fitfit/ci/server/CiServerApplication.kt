adcc7766ff235fb416dce2787bbdb60008a9b0a8
sj <sebastianthegreatful@gmail.com>
2018-07-12 00:33:58 +0200

    Add controller for git hooks

--- src/main/kotlin/dk/fitfit/ci/server/CiServerApplication.kt
+++ src/main/kotlin/dk/fitfit/ci/server/CiServerApplication.kt
@@ -2,10 +2,23 @@ package dk.fitfit.ci.server
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class CiServerApplication
 
 fun main(args: Array<String>) {
     runApplication<CiServerApplication>(*args)
+}
+
+@RestController
+@RequestMapping("/hooks")
+class HookController {
+    @PostMapping("/github")
+    fun github(@RequestBody payload: Any) {
+        println(payload.toString())
+    }
 }
